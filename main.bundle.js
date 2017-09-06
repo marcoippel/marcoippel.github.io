ebefad4fdbac31efe4e1ebe264f1450a089d7c1e
Marco Ippel <marco@ippelsoft.nl>
2017-09-06 10:55:19 +0200

    Fix: changed to angular project

---
+++ main.bundle.js
@@ -0,0 +1,158 @@
+webpackJsonp([1],{
+
+/***/ "../../../../../src async recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "../../../../../src async recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "body{\r\n    \r\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "  <div id=\"fb-root\"></div>\n  <!-- Navigation -->\n  <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark fixed-top\">\n    <div class=\"container\">\n      <a class=\"navbar-brand\" href=\"#\">Afvalkalender</a>\n      <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\"\n        aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n          <span class=\"navbar-toggler-icon\"></span>\n        </button>\n      <div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">\n        <ul class=\"navbar-nav ml-auto\">\n          <li class=\"nav-item active\">\n            <a class=\"nav-link\" href=\"#\">Home\n                <span class=\"sr-only\">(current)</span>\n              </a>\n          </li>\n          <li class=\"nav-item\">\n            <a class=\"nav-link\" href=\"#\">About</a>\n          </li>\n          <li class=\"nav-item\">\n            <a class=\"nav-link\" href=\"#\">Services</a>\n          </li>\n          <li class=\"nav-item\">\n            <a class=\"nav-link\" href=\"#\">Contact</a>\n          </li>\n        </ul>\n      </div>\n    </div>\n  </nav>\n\n  <header class=\"masthead\">\n    <div class=\"overlay\">\n      <!-- \n      Zoetermeer \n      Lopik\n      Ede\n      Renkum\n      Veenendaal\n      Wageningen\n      Ermelo\n      Putten\n      Breda\n      Tilburg\n      Rhenen\n      Werkendam\n      Kampen\n      Moerdijk\n      Krimpen aan den IJssel\n      Assen\n      Loon op zand\n      Dongen\n      Oss\n      Utrecht\n      Hilvarenbeek \n      Amstelveen\n      Hoogezand-Sappemeer\n      Dronten\n      Amerongen\n      Doorn \n      Driebergen-Rijsenburg \n      Leersum\n      Maarn\n      stichtsevecht\n      Teylingen\n      Westervoort\n      Noordenveld\n      Leusden\n      Harderwijk \n      Oldebroek \n      Elburg\n      Nunspeet\n      Waalwijk\n      Gilze en Rijen\n      Renkum\n      Veendam\n      Leudal\n      Amsterdam Centrum\n      Lansingerland \n      Haarlem\n      Wageningen\n      Etten-Leur\n      Dongen\n      Aalburg\n      Borger Odoorn\n      Aa en Hunze\n      Vlagtwedde\n      Barneveld\n      Oosterhout\n      Smallingerland\n      Leusden\n      Pekela\n      Lingewaard\n      Amersfoort\n      Stadskanaal\n      Maasgouw\n      Ede\n      Slochteren\n      Tynaarlo\n      Rheden\n      Roermond\n      nieuwegein\n      baarn\n      soest\n      zeist\n      ijselsteijn \n      Bergeijk\n      Best\n      Bladel\n      Boekel\n      Boxmeer\n      Buren\n      Cuijk\n      Culemborg\n      Echt-Susteren\n      Eersel\n      Geldermalsen\n      Grave\n      Helmond\n      Heumen\n      Lingewaal\n      Maasdriel\n      Mill en Sint Hubert\n      Neder-Betuwe\n      Neerijnen\n      Nuenen\n      Oirschot\n      Reusel-De Mierden\n      Sint Anthonis\n      Someren\n      Son en Breugel\n      Terneuzen\n      Tiel\n      Veldhoven\n      West Maas en Waal\n      Zaltbommel\n      -->\n      <div class=\"container\">\n        <h1 class=\"display-1 text-white\">Afvalkalender chatbot</h1>\n        <h2 class=\"display-4 text-white\">Nooit meer te laat met het afval buiten zetten!</h2>\n      </div>\n    </div>\n  </header>\n\n  <section>\n    <div class=\"container\">\n      <div class=\"row align-items-center\">\n        <div class=\"col-md-6 order-2\">\n          <div class=\"p-5\">\n\n            <img class=\"img-fluid\" src=\"./../assets/images/animation.gif\" alt=\"\">\n          </div>\n        </div>\n        <div class=\"col-md-6 order-1\">\n          <div class=\"p-5\">\n            <h2 class=\"display-4\">Hoe werkt het</h2>\n            <p>Wil je weten welke afvalsoort er wanneer naar buiten moet? Vraag het de afvalkalender chatbot. Op basis van je\n              postcode en huisnummer kan de afvalkalender chatbot je vertellen welke afvalsoort er wanneer wordt opgehaald.\n            </p>\n          </div>\n        </div>\n      </div>\n    </div>\n  </section>\n\n  <section>\n    <div class=\"container\">\n      <div class=\"row align-items-center\">\n        <div class=\"col-md-6\">\n          <div class=\"p-5\">\n            <img class=\"img-fluid rounded-circle\" src=\"./../assets/images/Pushbericht.png\" alt=\"\">\n          </div>\n        </div>\n        <div class=\"col-md-6\">\n          <div class=\"p-5\">\n            <h2 class=\"display-4\">Push notificaties</h2>\n            <p>Met de afvalkalender chatbot is het mogelijk om notificaties te ontvangen als er afval wordt opgehaald. U krijgt\n              de avond ervoor om 19:00 uur een berichtje op uw facebook messenger over welke afvalsoort er wordt opgehaald.</p>\n          </div>\n        </div>\n      </div>\n    </div>\n  </section>\n\n  <section>\n    <div class=\"container\">\n      <div class=\"row align-items-center\">\n        <div class=\"col-md-6 order-2\">\n          <div class=\"p-5\">\n            <img class=\"img-fluid rounded-circle\" src=\"./../assets/images/messenger_code.png\" alt=\"\">\n          </div>\n        </div>\n        <div class=\"col-md-6 order-1\">\n          <div class=\"p-5\">\n            <h2 class=\"display-4\">Let's chat!</h2>\n            <p>Om de chat te beginnen scan de code met facebook messenger of gebruik de onderstaande knop.</p>\n\n          </div>\n        </div>\n      </div>\n    </div>\n  </section>\n\n  <!-- Footer -->\n  <footer class=\"py-5 bg-dark\">\n    <div class=\"container\">\n      <p class=\"m-0 text-center text-white\">Copyright &copy; Your Website 2017</p>\n    </div>\n    <!-- /.container -->\n  </footer>\n\n  \n\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
